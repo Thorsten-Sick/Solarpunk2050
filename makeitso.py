29ffd1946a4d73ca8a1f4d78d815807a12e17094
Thorsten Sick <thorsten.sick@email.de>
2025-11-14 20:36:55 +0100

    everything can be compiled now

--- makeitso.py
+++ makeitso.py
@@ -19,6 +19,7 @@ def compile(texdir, texfile, largs):
         subprocess.run(command, cwd=texdir, shell=True, check=False, capture_output=False)
     # if os.path.isfile(pdffilename):
         #     shutil.copy2()
+    print(f"Done {texdir}  {texfile}")
 
 def func_compile(largs):
     """Dispatch the compile calls."""
@@ -29,9 +30,25 @@ def func_compile(largs):
         compile(texdir, texfile, largs)
 
     if largs.flohmarkt or largs.all:
-        texdir = "en/sourcebook"
-        texfile = "sourcebook.tex"
+        texdir = "en/adventure_sammlung_de"
+        texfile = "standalone_flohmarkt_de.tex"
         compile(texdir, texfile, largs)
+
+    if largs.fleamarket or largs.all:
+        texdir = "en/adventure_sammlung_de"
+        texfile = "standalone_flohmarkt_en.tex"
+        compile(texdir, texfile, largs)
+
+    if largs.world_destroying_machine or largs.all:
+        texdir = "en/con_adventure_1"
+        texfile = "standalone.tex"
+        compile(texdir, texfile, largs)
+
+    if largs.pyramid or largs.all:
+        texdir = "en/adventure_pyramids"
+        texfile = "standalone.tex"
+        compile(texdir, texfile, largs)
+
 
 def create_parser():
     """ Creates the parser for the command line arguments"""
@@ -46,9 +63,11 @@ def create_parser():
     parser_run.set_defaults(func=func_compile)
     parser_run.add_argument("-s", "--sourcebook", action="store_true", help="compile the sourcebook")
     parser_run.add_argument("-a", "--all", action="store_true", help="compile all books")
-    parser_run.add_argument("-f", "--flohmarkt", action="store_true", help="compile the flohmarkt")
+    parser_run.add_argument("-f", "--flohmarkt", action="store_true", help="compile the flohmarkt (DE)")
+    parser_run.add_argument("-F", "--fleamarket", action="store_true", help="compile the fleamarket (EN)")
+    parser_run.add_argument("-w", "--world_destroying_machine", action="store_true", help="compile the world destroying machine")
+    parser_run.add_argument("-p", "--pyramid", action="store_true", help="compile the berlin pyramid")
     parser_run.add_argument("--strict", action="store_true", help="compile strict, do not ignore warnings")
-
 
     return lparser
 
