e59814e2a03052fd00361cb3d8231cb8fdde9b07
Thorsten Sick <thorsten.sick@email.de>
2026-04-11 10:28:01 +0200

    grt webpage 2026

---
+++ homepage/solarpunk2050.com/grt2026.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta description="A generator for Solarpunk 2050 Lost NPCs and names">
+    <meta name="keywords" content="solarpunk, ttrpg, rpg, pen&paper, rollenspiel, fate, hopepunk, hope, utopia, name generator, character generator">
+    <title>Solarpunk 2050 - The Lost</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/static/styles.css">
+  </head>
+
+
+  <body>
+    <?php
+    include "navbar.php"
+    ?>
+
+    <?php
+    $appearance = Array("Traditional clothes", "Workshop clothes", "Hunting gear", "Full beard", "Long, unwashed hair", "Callus on the hands");
+    $name = Array("Caesar", "Elisabeth", "George", "Friedrich the small", "Albert", "Marie");
+    $job = Array("Librarian and Priest", "Hunter", "Field cook", "Cook", "Woodworker", "Smith", "Car repair man", "Guard", "Expedition member", "First responder", "Farmer", "Goat herder", "Animal trainer");
+    $event = Array("Evening ritual: Story time", "Classical theatre", "Jam session", "Open Pot");
+    $location = Array("A camp in the woods", "An old restaurant, renovated", "Ruins and explorers", "A tent", "A caravan trek", "The high secure library-bus");
+    $food = Array("Deer", "Deer, wild potatoes and mushroom sauce", "Goose, in clay coat", "Vegetable-inna-pot", "German Gumbo", "Goat cheese with fresh bread and herbs");
+    ?>
+
+<div class="jumbotron vertical-center">
+    <div class="container">
+    <div class="container pt-5">
+
+    <center>Das Startabenteuer "Die Weltvernichtungsmaschine", ein spannendes Con-size Abenteuer für bis zu 6 Spielern.
+    Finde heraus, was die mysteriöse "Weltvernichtungsmaschine" ist und führe sie dem Recycling zu.</center>
+    <p>
+    <center>
+      <a href="downloads/quickstart_solarpunk_2050_die_weltvernichtungsmaschine.pdf" download="quickstart_solarpunk_2050_die_weltvernichtungsmaschine.pdf">Download</a>
+    </center>
+    </p>
+
+    <a href="downloads/quickstart_solarpunk_2050_die_weltvernichtungsmaschine.pdf" download="quickstart_solarpunk_2050_die_weltvernichtungsmaschine.pdf">
+      <center><img src="images/Solarpunk-Die_Weltvernichtungsmaschine.png" alt="Titelbild: Die Weltvernichtungsmaschine" width=400></center>
+    </a>
+
+
+    </div>
+    </div>
+</div>
+
+  </body>
+</html>
